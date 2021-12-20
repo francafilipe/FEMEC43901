1f96d474f5e68f59a5e313c36190f127f693db75
Filipe <filipeffranc@gmail.com>
2021-12-20 08:38:24 -0300

    Create function to run implemented algorithms

---
+++ evaluate.py
@@ -0,0 +1,33 @@
+# File used to test the implemented algorithms for each optimization method
+
+from SpaceReduction import *
+
+print('Função Default - Exemplo Apostila')
+print('Bisseção: ',     bisseccao('Default',[0, 600],0.2,1e-3,100))
+print('Seção Aurea: ',  golden_ratio('Default',[0, 600],1e-3,100))
+print('Fibonacci: ',    fibonacci_method('Default',[0, 600],1e-3,100))
+print('-----------------------')
+
+print('Função Sphere (y=0)')
+print('Bisseção: ',     bisseccao('Sphere',[-1e3, 1e3],0.5,1e-3,100))
+print('Seção Aurea: ',  golden_ratio('Sphere',[-1e3, 1e3],1e-3,100))
+print('Fibonacci: ',    fibonacci_method('Sphere',[-1e3, 1e3],1e-3,100))
+print('-----------------------')
+
+print('Função Beale (y=0.5)')
+print('Bisseção: ',     bisseccao('Beale',[-4.5, 4.5],0.2,1e-3,100))
+print('Seção Aurea: ',  golden_ratio('Beale',[-4.5, 4.5],1e-3,100))
+print('Fibonacci: ',    fibonacci_method('Beale',[-4.5, 4.5],1e-3,100))
+print('-----------------------')
+
+print('Função Three-Hump Camel (y=0)')
+print('Bisseção: ',     bisseccao('Beale',[-5, 5],0.5,1e-3,100))
+print('Seção Aurea: ',  golden_ratio('Beale',[-5, 5],1e-3,100))
+print('Fibonacci: ',    fibonacci_method('Beale',[-5, 5],1e-3,100))
+print('-----------------------')
+
+print('Função Rastrigin (y=0)')
+print('Bisseção: ',     bisseccao('Rastrigin',[-5.12, 5.12],0.5,1e-3,100))
+print('Seção Aurea: ',  golden_ratio('Rastrigin',[-5.12, 5.12],1e-3,100))
+print('Fibonacci: ',    fibonacci_method('Rastrigin',[-5.12, 5.12],1e-3,100))
+print('-----------------------')
