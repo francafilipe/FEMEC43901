9575a8e66bf602b73b707c2e3885a8f98a510be0
Filipe <filipeffranc@gmail.com>
2022-01-10 00:09:31 -0300

    Implement scripts to evaluate the test function in all different methods studied in week 2

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,75 @@
 # File used to test the implemented algorithms for each optimization method
 
+from numpy import poly1d
 from Unconstrained1DMethods import *
+import matplotlib.pyplot as plt
 
+
+"""
+# Evaluate the Polynomial Approximation Method
+# Simulation&Optimization Parameters
+func = 'Rastrigin'
+intv = [-5.12, 5.12]
+max_N = 6
+plot_styles = ['go','r^','bv','ms','ch']
+y_real = ZDT(x=linspace(intv[0],intv[1],500),func=func)
+plt.plot(linspace(intv[0],intv[1],500),y_real,'k-')
+
+for N in range(2,max_N+1):
+    sol, p = polinomialApproximation(function=func,interval=intv,N=N)
+    poly = poly1d(p)
+    x = linspace(intv[0],intv[1],50)
+    y = poly(x)
+    plt.plot(x,y,plot_styles[N-2])
+
+plt.suptitle('Aproximação Polinomial p/ função ' + func + '\n Intervalo = ' + str(intv), fontweight='bold')
+plt.ylabel('Valor da função f(x)'); plt.xlabel('Valor da variável de projeto x');
+plt.legend(('Função f','n = 2','n = 3', 'n = 4', 'n = 5', 'n = 6'))
+plt.show()
+"""
+
+"""
+# Evaluate test functions for Newton, Levenberg-Marquardt & Quasi-Newton Methods
+
+
+func = 'Rastrigin'
+x0 = -0.1
+N = 15
+tol = 1e-8
+h = 1e-4
+
+optimal1, iterations1, x1, f1 = Newton(function=func,initial=x0,tol=tol,N=N,h=h)
+optimal2, iterations2, x2, f2 = LevenbergMarquardt(function=func,x0=x0,tol=tol,N=N,h=h)
+optimal3, iterations3, x3, f3 = Quasi_Newton(function=func,x0=x0,xp=0.1,tol=tol,N=N,h=h)
+
+x1[iterations1:-1] = optimal1[0]; x1[-1] = optimal1[0]
+x2[iterations2:-1] = optimal2[0]; x2[-1] = optimal2[0]
+x3[iterations3:-1] = optimal3[0]; x3[-1] = optimal3[0]
+
+f1[iterations1:-1] = optimal1[1]; f1[-1] = optimal1[1]
+f2[iterations2:-1] = optimal2[1]; f2[-1] = optimal2[1]
+f3[iterations3:-1] = optimal3[1]; f3[-1] = optimal3[1]
+
+
+# Plot the Results
+iterations = range(1,N+1)
+
+figure1, plots = plt.subplots(2,1)
+
+plots[0].plot(iterations, f1, 'b--',iterations, f2, 'b-.', iterations, f3, 'bo')
+plots[1].plot(iterations, x1, 'b--', iterations, x2, 'b-.', iterations, x3, 'bo')
+plt.suptitle('Convergência dos Métodos p/ função ' + func + '\n Tolerância = 10^-8 & Chute inicial = ' + str(x0), fontweight='bold')
+plots[0].set_ylabel('Valor da função f(x)'); plots[1].set_ylabel('Valor da variável de projeto x');
+plots[0].set_xlabel('N° de iterações'); plots[1].set_xlabel('N° de iterações') 
+plt.setp(plots, xticks=range(0,N+1))
+plots[0].legend(('Newton','Levenberg-Marquardt','Quasi-Newton'))
+
+plt.show()
+"""
+
+
+"""
+# Evaluate test functions for Bissection, Golden Ratio & Fibonacci Methods
 print('Função Default - Exemplo Apostila')
 print('Bisseção: ',     bisseccao('Default',[0, 600],0.2,1e-8,100))
 print('Seção Aurea: ',  golden_ratio('Default',[0, 600],1e-8,100))
@@ -31,3 +99,4 @@ print('Bisseção: ',     bisseccao('Rastrigin',[-5.12, 5.12],0.5,1e-3,100))
 print('Seção Aurea: ',  golden_ratio('Rastrigin',[-5.12, 5.12],1e-3,100))
 print('Fibonacci: ',    fibonacci_method('Rastrigin',[-5.12, 5.12],1e-3,100))
 print('-----------------------')
+"""
