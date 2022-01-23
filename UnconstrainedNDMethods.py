917cb3e49619a5021829e2abe0171eb38d686985
Filipe <filipeffranc@gmail.com>
2022-01-22 21:16:09 -0300

    Implemente Lev-Marquant method for 2 variable functions (not all test functions are working yet)

--- UnconstrainedNDMethods.py
+++ UnconstrainedNDMethods.py
@@ -196,8 +196,8 @@ def newton2D(function,x0,tol=1e-3,itermax=100,runitermax=False):
         d2f_k = array([ [d2f[0][0](xsol[k,0],xsol[k,1]), d2f[0][1](xsol[k,0],xsol[k,1])], [d2f[1][0](xsol[k,0],xsol[k,1]), d2f[1][1](xsol[k,0],xsol[k,1])] ])
 
         # Create parametric function f = f(a) = f( x0 - a*df/dx|x0, y0 - a*df/dy|y0 )
-        x_k = xsol[k,0] - a*inv(d2f_k)*df_k
-        fn_symb = f( x_k[0], x_k[1] )[0]
+        x_k = xsol[k,0] - a*dot(inv(d2f_k),df_k)
+        fn_symb = f( x_k[0], x_k[1] )
         fn = lambdify(a,fn_symb,"numpy")
 
         # Optimize the function fn for \alhpa (a)
@@ -218,6 +218,53 @@ def newton2D(function,x0,tol=1e-3,itermax=100,runitermax=False):
     return solution
 
 
+def levenbergMarquardt2D(function,x0,lamb0=0,tol=1e-3,itermax=100,runitermax=False):
+    from sympy.abc import x, y, a
+    f   = function(x=x,y=y)
+    df  = [f.diff(x), f.diff(y)]
+    d2f = [ [f.diff(x,2), f.diff(x,y)] , [f.diff(y,x), f.diff(y,2)] ]
+    f   = lambdify([x,y],f,"numpy")
+    df  = [lambdify([x,y],df[0],"numpy"), lambdify([x,y],df[1],"numpy")]
+    d2f = [ [lambdify([x,y],d2f[0][0],"numpy"), lambdify([x,y],d2f[0][1],"numpy")], [lambdify([x,y],d2f[1][0],"numpy"), lambdify([x,y],d2f[1][1],"numpy")] ] 
+
+    lamb = zeros((itermax+1,))
+    lamb[0] = lamb0
+    xsol = zeros((itermax+1,2))
+    xsol[0,:] = x0
+
+    k = 0
+    while (k < itermax):
+        print('\n Starting ' + str(k+1) + '° iteration: ')
+
+        # Calculate the gradient & hassian of the function for x[k]
+        df_k  = array([ df[0](xsol[k,0],xsol[k,1]),  df[1](xsol[k,1],xsol[k,1]) ])
+        d2f_k = array([ [d2f[0][0](xsol[k,0],xsol[k,1]), d2f[0][1](xsol[k,0],xsol[k,1])], [d2f[1][0](xsol[k,0],xsol[k,1]), d2f[1][1](xsol[k,0],xsol[k,1])] ])
+
+        # Create parametric function f = f(a) = f( x0 - a*df/dx|x0, y0 - a*df/dy|y0 )
+        x_k = xsol[k,0] - a*dot(( inv(d2f_k) + lamb[k]*identity(2) ), df_k)
+        fn_symb = f( x_k[0], x_k[1] )
+        fn = lambdify(a,fn_symb,"numpy")
+
+        # Optimize the function fn for \alhpa (a)
+        a_optimal, __, __, __ = Newton(function=fn,x0=0,tol=tol,N=itermax,h=1e-4)
+        alpha = a_optimal[0]
+
+        # Update the initial guess value (x0 & y0)
+        xsol[k+1,:] = xsol[k,:] - alpha*dot(( inv(d2f_k) + lamb[k]*identity(2) ), df_k)
+        lamb[k+1]   = abs( f(xsol[k+1,0],xsol[k+1,1]) -  f(xsol[k,0],xsol[k,1]) )/abs( f(xsol[k,0],xsol[k,1]) )
+        k = k+1
+
+        if ( (sum(abs(xsol[k,:] - xsol[k-1,:])) <= tol) and (not runitermax) ):
+            solution = xsol[k,:]
+            break
+
+    else:
+        solution = xsol
+
+    return solution
+
+
+
 """
 sol = powell(sphere,x0=[-5, 5],tol=1e-3,itermax=100,runitermax=True)
 print('\n',sol,'\n')
@@ -234,7 +281,13 @@ sol = direcoesConjugadas(beale,x0=[5, 5],tol=1e-3,itermax=50,runitermax=True)
 print('\n',sol,'\n')
 """
 
+
 """
 sol = newton2D(sphere,x0=[5, 5],tol=1e-3,itermax=10,runitermax=True)
+print('\n',sol,'\n')
+"""
+
+"""
+sol = levenbergMarquardt2D(sphere,x0=[5, 5],lamb0=0,tol=1e-3,itermax=100,runitermax=True)
 print('\n',sol,'\n')
 """
