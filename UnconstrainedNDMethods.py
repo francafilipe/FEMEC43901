c0ad38e26a5905627b9198575c99700a30da377c
Filipe <filipeffranc@gmail.com>
2022-01-22 21:00:47 -0300

    Implement 2 variables newton method (not working for all test functions yet)

--- UnconstrainedNDMethods.py
+++ UnconstrainedNDMethods.py
@@ -1,7 +1,9 @@
+from cProfile import run
 from sympy import Symbol, lambdify
-from numpy import array, concatenate, gradient, identity, linspace, newaxis, polyfit, polyder, roots, zeros, size, argmin, eye, inner, transpose, delete
+from numpy import array, concatenate, gradient, identity, linspace, newaxis, polyfit, polyder, roots, zeros, size, argmin, eye, inner, transpose, delete, dot
 from numpy.random import rand
 from numpy.lib.polynomial import polyder
+from numpy.linalg import inv
 from Unconstrained1DMethods import *
 from support_funcs import *
 from test_functions import *
@@ -103,7 +105,7 @@ def maximaDescida(function,x0,tol=1e-3,itermax=100,runitermax=False):
         # Create parametric function f = f(a) = f( x0 - a*df/dx|x0, y0 - a*df/dy|y0 )
         fn_symb = f( xsol[k,0] - a*df[0](xsol[k,0],xsol[k,1]), xsol[k,1] - a*df[1](xsol[k,1],xsol[k,1]) )
         fn = lambdify(a,fn_symb,"numpy")
-
+        print(fn_symb)
         # Optimize the function fn for \alhpa (a)
         a_optimal, __, __, __ = Newton(function=fn,x0=0,tol=tol,N=itermax,h=1e-4)
 
@@ -170,17 +172,69 @@ def direcoesConjugadas(function,x0,tol=1e-3,itermax=100,runitermax=False):
     return solution
 
 
+
+# Metodos de Segunda Ordem (Newton, Levenberg-Marquardt, Variável Métrica)
+
+def newton2D(function,x0,tol=1e-3,itermax=100,runitermax=False):
+    from sympy.abc import x, y, a
+    f   = function(x=x,y=y)
+    df  = [f.diff(x), f.diff(y)]
+    d2f = [ [f.diff(x,2), f.diff(x,y)] , [f.diff(y,x), f.diff(y,2)] ]
+    f   = lambdify([x,y],f,"numpy")
+    df  = [lambdify([x,y],df[0],"numpy"), lambdify([x,y],df[1],"numpy")]
+    d2f = [ [lambdify([x,y],d2f[0][0],"numpy"), lambdify([x,y],d2f[0][1],"numpy")], [lambdify([x,y],d2f[1][0],"numpy"), lambdify([x,y],d2f[1][1],"numpy")] ] 
+
+    xsol = zeros((itermax+1,2))
+    xsol[0,:] = x0
+
+    k = 0
+    while (k < itermax):
+        print('\n Starting ' + str(k+1) + '° iteration: ')
+
+        # Calculate the gradient & hassian of the function for x[k]
+        df_k  = array([ df[0](xsol[k,0],xsol[k,1]),  df[1](xsol[k,1],xsol[k,1]) ])
+        d2f_k = array([ [d2f[0][0](xsol[k,0],xsol[k,1]), d2f[0][1](xsol[k,0],xsol[k,1])], [d2f[1][0](xsol[k,0],xsol[k,1]), d2f[1][1](xsol[k,0],xsol[k,1])] ])
+
+        # Create parametric function f = f(a) = f( x0 - a*df/dx|x0, y0 - a*df/dy|y0 )
+        x_k = xsol[k,0] - a*inv(d2f_k)*df_k
+        fn_symb = f( x_k[0], x_k[1] )[0]
+        fn = lambdify(a,fn_symb,"numpy")
+
+        # Optimize the function fn for \alhpa (a)
+        a_optimal, __, __, __ = Newton(function=fn,x0=0,tol=tol,N=itermax,h=1e-4)
+        alpha = a_optimal[0]
+
+        # Update the initial guess value (x0 & y0)
+        xsol[k+1,:] = xsol[k,:] - alpha*dot(inv(d2f_k),df_k)
+        k = k+1
+
+        if ( (sum(abs(xsol[k,:] - xsol[k-1,:])) <= tol) and (not runitermax) ):
+            solution = xsol[k,:]
+            break
+
+    else:
+        solution = xsol
+
+    return solution
+
+
 """
 sol = powell(sphere,x0=[-5, 5],tol=1e-3,itermax=100,runitermax=True)
 print('\n',sol,'\n')
 """
 
+
 """
-sol = maximaDescida(rastrigin,x0=[5, 5],tol=1e-3,itermax=100)
+sol = maximaDescida(sphere,x0=[5, 5],tol=1e-3,itermax=100)
 print('\n',sol,'\n')
 """
 
 """
 sol = direcoesConjugadas(beale,x0=[5, 5],tol=1e-3,itermax=50,runitermax=True)
+print('\n',sol,'\n')
+"""
+
+"""
+sol = newton2D(sphere,x0=[5, 5],tol=1e-3,itermax=10,runitermax=True)
 print('\n',sol,'\n')
 """
